50249cd7c78fe014e797c7875c17e0c75851f177
Andrei Victor Bulearca <andrei.bulearca@gmail.com>
2020-12-19 18:35:32 +0200

    add google analytics tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 import { VERSION } from '@angular/core';
 import { VERSION as AngularMaterialVersion } from '@angular/material/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { environment } from '../environments/environment';
+
+// declare gives Angular app access to ga function
+// tslint:disable-next-line:ban-types
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -13,4 +18,13 @@ export class AppComponent {
   appVersion: string = environment.appVersion;
   angularVersion: string = VERSION.full;
   angularMaterialVersion: string = AngularMaterialVersion.full;
+
+  constructor(public router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        console.log('NavigationEnd: ', event.urlAfterRedirects);
+        gtag('config', environment.gaMeasurementId, {page_path: event.urlAfterRedirects});
+      }
+    });
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  appVersion: require('../../package.json').version
+  appVersion: require('../../package.json').version,
+  gaMeasurementId: 'G-B3JBT7SCC8'
 };
